ae0924392c8469c10be13bfd6f373b2312ea1c3f
ChinKF99 <apb2657@gmail.com>
2024-12-05 13:05:27 +0800

    Create Grid layout using Flexbox

--- sketch.js
+++ sketch.js
@@ -1,30 +1,27 @@
-const container = document.querySelector("#container");
-//let rows = document.getElementsByClassName("gridRow");
-//let cells = document.getElementsByClassName("cell");
+const container = document.querySelector('.container');
 
-
-function createGrid(){
-    createRow(16)
-    //createColumn(16)
+function createGrid() {
+    for (let i = 0; i < 16; i++) {        
+        const createDivRow = document.createElement("div");
+        createDivRow.classList.add("gridrow")
+        container.appendChild(createDivRow);
+    }
+    
 }
 
-function createRow(rowNum){
-    for (let r=0 ; r < rowNum; r++){
-        let row = document.createElement("div")
-        row.classList.add("gridRow")
-        container.appendChild(row)
-    }
-}
-
-function createColumn(cellNum){
-    for (let i=0 ; i < rows.length; i++){
-        for (let c = 0; c < cellNum; c++){
-            let newCell = document.createElement("div")
-            newCell.classList.add("cell")
-            rows[c].appendChild(newCell)
-
-        }
-    }
+function createGridCells() {  
+    const divRows = document.querySelectorAll(".gridrow");    
+    //iterate over each one of them
+    divRows.forEach(divRow =>{
+        //and append to it a new div for as many times as the value fetched from the input element
+        for(let i=0;i < 16; i++){
+          const createDivCell = document.createElement("div");
+          divRow.appendChild(createDivCell);
+        }        
+    });       
 }
 
 createGrid()
+createGridCells()
+
+// https://stackoverflow.com/questions/78538355/creating-a-grid-using-javascript-without-displaygrid
