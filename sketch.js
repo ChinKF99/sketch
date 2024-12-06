69ecfa6e663bc5e534c5a30f886ea93e91696ee2
Chin <khaix.foong.chin@intel.com>
2024-12-06 16:39:16 +0800

    Added create button to create Grid

--- sketch.js
+++ sketch.js
@@ -1,38 +1,42 @@
-const container = document.querySelector('.container');
+const container = document.querySelector('.container')
+const input= document.querySelector(".input")
+const create = document.querySelector(".createGrid")
+const reset = document.querySelector(".reset")
 
-function createGrid() {
-    for (let i = 0; i < 16; i++) {        
+
+create.addEventListener("click",createGrid)
+create.addEventListener("click",createGridCells)
+// reset.addEventListener("click",()=>{
+//     const gridRow = document.querySelector("gridRow")
+//     gridRow.remove()
+// })
+
+function createGrid(gridNum) {
+    gridNum = Number(input.value)
+    for (let i = 0; i < gridNum; i++) {        
         const createDivRow = document.createElement("div");
-        createDivRow.classList.add("gridrow")
+        createDivRow.classList.add("gridRow")
         container.appendChild(createDivRow);
     }}
 
-function createGridCells() {  
-    const divRows = document.querySelectorAll(".gridrow");    
+function createGridCells(gridNum) {  
+    gridNum = Number(input.value)
+    const divRows = document.querySelectorAll(".gridRow");    
     //iterate over each one of them
     divRows.forEach(divRow =>{
         //and append to it a new div for as many times as the value fetched from the input element
-        for(let i=0;i < 16; i++){
+        for(let i=0;i < gridNum; i++){
           const createDivCell = document.createElement("div");
           createDivCell.classList.add("cell")
           divRow.appendChild(createDivCell);
         }        
-    });       
-}
+    });
 
-createGrid()
-createGridCells()
-
-const gridCell = document.querySelectorAll(".cell")
-
-gridCell.forEach(cell=>{
-    cell.addEventListener("mouseover",()=>{
-        cell.style.backgroundColor = "yellow"
-    })
-})
+    const gridCell = document.querySelectorAll(".cell")
+    gridCell.forEach(cell=>{
+        cell.addEventListener("mouseover",()=>{
+            cell.style.backgroundColor = "yellow"
+        })
+    })}
 
 
-
-
-
-// https://stackoverflow.com/questions/78538355/creating-a-grid-using-javascript-without-displaygrid
